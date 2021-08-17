cb0b51cda9a8148bbb86e8345bc93b3604544196
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-17 14:02:34 +0900

    ✨ getStaticProps を使った、データ有りの静的生成

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home(props) {
+  const { allPostsData } = props;
   return (
     <Layout home>
       <Head>
@@ -10,10 +12,33 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello</p>
-        <p>
-          Study Next.js
-        </p>
+        <p>Study Next.js</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
-  )
+  );
+}
+
+// getStaticProps:データ取得
+// 下記のコードでSSGの準備完了。index.jsがビルドされるタイミングでgetStaticPropsの関数が走る
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
